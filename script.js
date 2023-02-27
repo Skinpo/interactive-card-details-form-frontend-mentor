cdb7aedf6aacfa57f703b91c622ac107306452f2
celestine <lestine603@gmail.com>
2023-02-27 13:02:55 +0100

    more debugging on style for both desktop and mobile view

---
+++ script.js
@@ -0,0 +1,20 @@
+// const fullName = document.querySelector(".name");
+// const number = document.querySelector(".number");
+// const month = document.querySelector(".mm");
+// const year = document.querySelector(".yy");
+// const cvcDigit = document.querySelector(".cvc-digit")
+
+// let allInput = [fullName, number, month, year, cvcDigit];
+
+// console.log(allInput);
+
+// allInput.forEach(input => {
+//     input.addEventListener("keyup", () => {
+//         input.style.border = "2px solid #600594";
+//         console.log("input");
+//     })
+// })
+
+
+
+
